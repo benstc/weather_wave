4c1a26fb85a38218f3a6088dc19129f69642dee8
benstc <benstclair77@gmail.com>
2024-05-23 16:02:58 -0500

    comments and small changes to graph

--- datavisualization.py
+++ datavisualization.py
@@ -5,25 +5,28 @@ import numpy as np
 
 
 def visualizedata(equation):
+    # setting graph parameters
     plt.rcParams["figure.figsize"] = [15.00, 7]
     plt.rcParams["figure.autolayout"] = True
+
+    # setting up data from csv file
     columns = ["DATE","PRCP","SNOW","SNWD","TAVG","TMAX","TMIN"]
     df = pd.read_csv("data/newCSV.csv", usecols=columns)
 
+    # constructing sin curve based on input equation variables
     t = np.linspace(0, 730, num=200)
     amplitude = equation[0]
-    frequency = 2*np.pi/365
+    frequency = 2*np.pi/365 # set so there is one cycle per year
     phase_angle = equation[1]
     vert_shift = equation[2]
-
     y1 = amplitude * np.sin(frequency*t + phase_angle) + vert_shift
+    # Plotting data
     plt.plot(t, y1)
     plt.plot(df.DATE, df.TAVG)
     plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=50))
+    # Adding axis labels
+    plt.xlabel('Date')
+    plt.ylabel('Temperature (F)')
+    # Adding title to graph
+    plt.title('Sine Curve Over Weather Data')
     plt.show()
-
-def main():
-    visualizedata()
-
-if __name__ == "__main__":
-    main()
--- weatherwave.py
+++ weatherwave.py
@@ -4,25 +4,21 @@ import random
 
 from datavisualization import visualizedata
 
-# I want the array of average temperatures to be accessible by all methods that need to use it without
-# giving the array as an input to any of the methods
-
-# I should make a class that holds the cost, neighbors, randomequation and localminimum functions
-# that has the temps array as a member variable so that all of the equations can access it?
-
-W = (2*math.pi)/365   # should it be 365.25 for leap year
+# holds frequency variable for sin function
+W = (2*math.pi)/365 # set so that there is one cycle per year
 
 class weatherwave():
     def __init__(self, avgtemps):
         self.avgtemps = avgtemps
 
-    def cost(self, equation): # should I have the A, o, h vars input as an array?? probably...
+    def cost(self, equation):
         """
         this function will find the total difference between a given sin function
         specified with the input parameters and the average temperature throughout the year
         returning the float value that is the difference
         """
         cost = 0.0
+        # for each day in the temperature data, find the difference between sin function and temp
         for i in range(len(self.avgtemps)):
             sinval = equation[0]*math.sin(W*i + equation[1]) + equation[2]
             diff = abs(sinval-self.avgtemps[i])
@@ -32,7 +28,7 @@ class weatherwave():
 
 
 
-    def neighbors(self, equation): # return an array of arrays with all neighbors (3^3) for a given input of A, w, o and h
+    def neighbors(self, equation):
         """
         This function will find all of the neighbors of a given sin function,
         of which there will be (3^3 - 1), since each parameter will have 3 neighbors and there are 3 parameter (but we dont want
@@ -45,11 +41,11 @@ class weatherwave():
         o = equation[1]
         h = equation[2]
 
-        # change as see fit (need to change them as they are right now most definitely)
         adiff = 0.1
         odiff = 0.025
         hdiff = 0.1
 
+        # return all neighbors of a given equation
         neighbors = [[a, o, h+hdiff],
                      [a, o, h-hdiff],
                      [a, o+odiff, h],
@@ -96,7 +92,7 @@ class weatherwave():
         return [a, o, h]
 
 
-    def minimum(self, equation): # I think I want to add A, o, h parameters (as an array?) to this function to input the random equation at the start
+    def minimum(self, equation):
         """
         this function will contain the while loop and funciton calls to cost and neighbors
         """
@@ -139,26 +135,12 @@ def main():
     findsin = weatherwave(avgtemps=avgtemps)
     randomstart = findsin.randomequation()
     minimumequation = findsin.minimum(randomstart)
-    print(minimumequation)
-    print(findsin.cost(minimumequation))
+    print(f'optimal amplitude: {minimumequation[0]}')
+    print(f'optimal phase angle: {minimumequation[1]}')
+    print(f'optimum vertical shift: {minimumequation[2]}')
     visualizedata(minimumequation)
     return
 
 
 if __name__ == "__main__":
     main()
-
-# main will parse the newCSV.csv file and make an array of length 730 holding the average temperatures
-# for each day over the 2 year span that the data consists of
-
-# offload heavy lifting to the local minimum function 
-
-# want to start with a random sin equation in main based on ranges of random values from the doc
-
-# find the cost of this initial equation
-
-# get all neighbors of this equation and keep the equation with the lowest cost found
-
-# if the lowest neighbors cost is lower than the cost of the initial equation, set the neighbor equation to the initial
-
-# loop until the lowest neighbor cost is not lower than the cost of the initial equation, then return the initial equation (print the cost?)
